fc7d844bf3b2926ddfcbeeaf426422112fbd2c34
MuhdHafiz7 <hafizzainolasri@gmail.com>
2026-06-11 00:08:51 +0800

    fix cloudflare config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Cloudflare plugin only needed for Cloudflare Pages deployment
+// npm install @cloudflare/vite-plugin
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
